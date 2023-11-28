4e07d8c7d6be11426891a49aba502fbb2f59e277
William Jay Inclino <wjay.inclino@gmail.com>
2023-11-28 16:18:40 +0800

    add trappings: dispatcher can only dispatch his assigned dispatchedTeam

--- src/dispatch/dispatch.controller.ts
+++ src/dispatch/dispatch.controller.ts
@@ -17,6 +17,7 @@ export class DispatchController {
   @Post()
   @UsePipes(new ValidationPipe())
   async create(@Body() createDispatchDtos: CreateDispatchDto[] | CreateDispatchDto): Promise<Dispatch[]> {
+	console.log('create()')
     if (!Array.isArray(createDispatchDtos)) {
       throw new BadRequestException('Invalid input. Expected an array.');
     }
@@ -26,11 +27,13 @@ export class DispatchController {
 
 	@Get()
 	async findAll(): Promise<Dispatch[]> {
+		console.log('findAll()')
 		return await this.dispatchService.findAll();
 	}
 
 	@Get(':id')
 	async findOne(@Param('id') id: string): Promise<Dispatch> {
+		console.log('findOne()', id)
 		const dispatch = await this.dispatchService.findOne(id);
 		return dispatch;
 	}
@@ -41,6 +44,7 @@ export class DispatchController {
 		@Param('id') id: string,
 		@Body() updateDispatchDto: UpdateDispatchDto
 	): Promise<Dispatch> {
+		console.log('update()', id, updateDispatchDto)
 		const updatedDispatch = await this.dispatchService.update(id, updateDispatchDto);
 		return updatedDispatch;
 	}
@@ -49,14 +53,17 @@ export class DispatchController {
 	async updateTimeField(
 	  @Param('id', new ParseUUIDPipe()) dispatchId: string,
 	  @Param('fieldName') fieldName: string,
+	  @Body() updateDispatchDto: UpdateDispatchDto
 	): Promise<Dispatch> {
 		console.log('updateTimeField()', dispatchId, fieldName)
-	  return await this.dispatchService.updateTimeField(dispatchId, fieldName);
+		console.log('updateDispatchDto', updateDispatchDto)
+	  return await this.dispatchService.updateTimeField(dispatchId, fieldName, updateDispatchDto);
 	}
 
 	@Delete(':id')
 	@HttpCode(HttpStatus.NO_CONTENT)
 	async remove(@Param('id') id: string): Promise<void> {
+		console.log('remove()', id)
 		await this.dispatchService.remove(id);
 	}
 }
