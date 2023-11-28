ce0773827ea48c29d31ae5d784dcfcae9fa22325
William Jay Inclino <wjay.inclino@gmail.com>
2023-11-28 18:44:12 +0800

    add whitelist localhost:8000

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://cdrrmo-client:5173'],
+    origin: [
+      'http://localhost:5173', 'http://cdrrmo-client:5173',
+      'http://localhost:8000', 'http://cdrrmo-client:8000',
+    ],
     methods: '*',
     credentials: true,
   });
