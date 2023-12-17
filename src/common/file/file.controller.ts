2d40903407dde824dda2ac2805e3b5a91a5a973c
William Jay Inclino <wjay.inclino@gmail.com>
2023-12-17 20:38:52 +0800

    refactor file controller

--- src/common/file/file.controller.ts
+++ src/common/file/file.controller.ts
@@ -1,33 +1,21 @@
-import {
-  Controller,
-  Post,
-  UseInterceptors,
-  UploadedFile,
-  BadRequestException,
-  Query,
-  UsePipes,
-  ValidationPipe,
-  Get,
-  Res,
-  Param,
-  NotFoundException,
-} from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, BadRequestException, Get, Res, Param, NotFoundException, Logger, Query } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import * as path from 'path';
+import * as fs from 'fs';
 import * as fsPromises from 'fs/promises';
 import { extname } from 'path';
 import * as mimeTypes from 'mime-types';
-import * as fs from 'fs';
-
 
 @Controller('/api/v1/image')
 export class FileController {
+  private static readonly UPLOADS_PATH = path.join(process.cwd(), 'uploads');
+  private static logger = new Logger(FileController.name);
 
   @Get('/get-image/:filename')
   async getImage(@Res() res: Response, @Param('filename') filename: string) {
     try {
-      const imagePath = path.join(process.cwd(), 'uploads', filename);
+      const imagePath = path.join(FileController.UPLOADS_PATH, filename);
 
       // Check if the image file exists
       if (!fs.existsSync(imagePath)) {
@@ -43,7 +31,6 @@ export class FileController {
   }
 
   @Post()
-  @UsePipes(new ValidationPipe())
   @UseInterceptors(
     FileInterceptor('image', {
       storage: diskStorage({
@@ -55,64 +42,68 @@ export class FileController {
       },
     }),
   )
-
-
   async uploadFile(@UploadedFile() file, @Query('previousFilename') previousFilename) {
     try {
-      const uploadsPath = path.join(process.cwd(), 'uploads');
-
       // Check if the "uploads" directory exists, create it if not
-      try {
-        await fsPromises.access(uploadsPath);
-      } catch (error) {
-        await fsPromises.mkdir(uploadsPath, { recursive: true });
+      if (!fs.existsSync(FileController.UPLOADS_PATH)) {
+        fs.mkdirSync(FileController.UPLOADS_PATH, { recursive: true });
       }
 
       // Validate file type
       const mimeType = mimeTypes.lookup(file.originalname);
-      if (!mimeType || !['image/jpeg', 'image/png'].includes(mimeType)) {
-        throw new BadRequestException('Invalid file type. Only JPEG and PNG are allowed.');
+      if (!mimeType || !mimeType.startsWith('image/')) {
+        throw new BadRequestException('Invalid file type. Only images are allowed.');
       }
 
       // The file is now available on the local server, and you can save its information to the database or perform other actions.
 
       // Delete the previous file if a previous filename is provided
       if (previousFilename && previousFilename !== '') {
-        const previousFilePath = path.join(process.cwd(), 'uploads', previousFilename);
-        await fsPromises.unlink(previousFilePath);
+        const previousFilePath = path.join(FileController.UPLOADS_PATH, previousFilename);
+        await fsPromises.unlink(previousFilePath).catch((unlinkError) => {
+          if (unlinkError) {
+            FileController.logger.error(`Error deleting previous file '${previousFilePath}':`, unlinkError);
+          }
+        });
       }
 
       return { filename: file.filename };
     } catch (error) {
       // Log the error
-      console.error('Error uploading file:', error.message);
+      FileController.logger.error('Error uploading file:', error.message);
       throw error;
     }
   }
 
-  private static async getFileDestination(req, file, callback) {
-    const destination = path.join(process.cwd(), 'uploads');
-    console.log('Destination:', destination);
+  private static getFileDestination(req, file, callback) {
+    const destination = FileController.UPLOADS_PATH;
     callback(null, destination);
   }
 
   private static async getFileName(req, file, callback) {
     // Use the previous filename if available in the query parameters, otherwise generate a new one
     const previousFilename = req.query.previousFilename as string || '';
-    console.log('previousFilename', previousFilename);
     const timestamp = new Date().getTime();
     const uniqueFileName = `${file.originalname}-${timestamp}${extname(file.originalname)}`;
-
+  
     // Delete the previous file if a previous filename is provided
     if (previousFilename && previousFilename !== '') {
-      const previousFilePath = path.join(process.cwd(), 'uploads', previousFilename);
-      await fsPromises.unlink(previousFilePath).catch((unlinkError) => {
-        if (unlinkError) {
-          console.error('Error deleting previous file:', unlinkError);
+      const previousFilePath = path.join(FileController.UPLOADS_PATH, previousFilename);
+  
+      // Check if the file exists before attempting to unlink
+      try {
+        await fsPromises.access(previousFilePath);
+        await fsPromises.unlink(previousFilePath);
+      } catch (unlinkError) {
+        // Handle the error, or ignore if the file doesn't exist
+        if (unlinkError && unlinkError.code !== 'ENOENT') {
+          FileController.logger.error(`Error deleting previous file '${previousFilePath}':`, unlinkError);
         }
-      });
+      }
     }
-
+  
     callback(null, uniqueFileName);
   }
+  
+  
 }
