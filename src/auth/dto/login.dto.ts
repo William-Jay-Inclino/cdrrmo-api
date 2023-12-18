acbe88ac801f457cd4f3d1fcac0dddba524aed2a
William Jay Inclino <wjay.inclino@gmail.com>
2023-12-18 08:03:23 +0800

    remove username in create admin endpoint

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -4,10 +4,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  username: string;
+  // @ApiProperty()
+  // @IsString()
+  // @IsNotEmpty()
+  // username: string;
 
   @ApiProperty()
   @IsString()
