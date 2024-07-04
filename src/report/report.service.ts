789d79922140ffbb8500ea1cca92517f6942edf4
jay <wjay.inclino@gmail.com>
2024-07-04 14:05:46 +0800

    filter date time fields

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -37,14 +37,24 @@ export class ReportService {
     const result = [];
 
     for (const dateKey of allDates) {
+      // const dataForDate = dispatches.filter(
+      //   (dispatch) =>
+      //     dispatch.time_of_call.toISOString().split('T')[0] === dateKey,
+      // );
+
       const dataForDate = dispatches.filter(
-        (dispatch) =>
-          dispatch.time_of_call.toISOString().split('T')[0] === dateKey,
+        (dispatch) => {
+          const timeOfCall = new Date(dispatch.time_of_call);
+          return timeOfCall.toISOString().split('T')[0] === dateKey;
+        },
       );
+
+      const dataForDate_formatted_dateTime = this.formatDateTime(dataForDate)
 
       result.push({
         date: dateKey,
-        data: dataForDate,
+        // data: dataForDate,
+        data: dataForDate_formatted_dateTime,
       });
     }
 
@@ -62,4 +72,53 @@ export class ReportService {
 
     return allDates;
   }
+
+  private formatDateTime(dispatches: Dispatch[]): Dispatch[] {
+
+    console.log('formatDateTime()');
+
+    return dispatches.map(item => {
+      
+      // @ts-ignore
+      item.time_of_call = item.time_of_call ? this.formatDate(new Date(item.time_of_call)) : null
+      // @ts-ignore
+      item.time_proceeding_scene = item.time_proceeding_scene ?  this.formatDate(new Date(item.time_proceeding_scene)) : null
+      // @ts-ignore
+      item.time_arrival_scene = item.time_arrival_scene ? this.formatDate(new Date(item.time_arrival_scene)) : null
+      // @ts-ignore
+      item.time_proceeding_hospital = item.time_proceeding_hospital ? this.formatDate(new Date(item.time_proceeding_hospital)) : null
+      // @ts-ignore
+      item.time_arrival_hospital = item.time_arrival_hospital ? this.formatDate(new Date(item.time_arrival_hospital)) : null
+      // @ts-ignore
+      item.time_proceeding_base = item.time_proceeding_base ? this.formatDate(new Date(item.time_proceeding_base)) : null
+      // @ts-ignore
+      item.time_arrival_base = item.time_arrival_base ? this.formatDate(new Date(item.time_arrival_base)) : null
+      // @ts-ignore
+      item.created_at =  this.formatDate(new Date(item.created_at))
+
+      return item
+    })
+
+  }
+
+  private formatDate(date: Date) {
+    return (
+      [
+        this.padTo2Digits(date.getMonth() + 1),
+        this.padTo2Digits(date.getDate()),
+        date.getFullYear() % 100,
+      ].join('-') +
+      ' ' +
+      [
+        this.padTo2Digits(date.getHours()),
+        this.padTo2Digits(date.getMinutes()),
+        this.padTo2Digits(date.getSeconds()),
+      ].join(':')
+    );
+  }
+
+  private padTo2Digits(num: number) {
+    return num.toString().padStart(2, '0');
+  }
+
 }
